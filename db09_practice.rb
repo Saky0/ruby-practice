7de8b3a79daed12b9df5ff56941cbb51af669ade
Saky0 <daviteremattos@gmail.com>
2022-08-24 20:30:31 -0300

    New methods, 4), 5) and 6)

--- db09_practice.rb
+++ db09_practice.rb
@@ -1,6 +1,7 @@
 require 'mysql2'
 require 'dotenv/load'
 require_relative './methods.rb'
+require 'digest'
 $client = Mysql2::Client.new(host: "db09.blockshopper.com",
                              username: ENV['DB09_LGN'],
                              password: ENV['DB09_PWD'],
@@ -16,4 +17,15 @@ $client = Mysql2::Client.new(host: "db09.blockshopper.com",
 # get_class_subjects('100', $client)
 
 # 3) get_teachers_list_by_letter which will get a letter and return a list of teachers with their subjects in the next format:
-get_teachers_list_by_letter('a', $client)
+# get_teachers_list_by_letter('a', $client)
+
+# 4) Add new column md5 (char32) and make the necessary actions to fill every field
+# add_md5_column($client)
+# set_md5($client)
+
+# 5) Create method get_class_info, which will get class ID an return next informartion:
+# puts get_class_info(1, $client)
+
+# 6) Create method get_teachers_by_year which will get a year and return result in the next format:
+# "Teachers born in <YEAR>: <teacher name 1>, <teacher name 2> etc."
+puts get_teachers_by_year(1965, $client)
--- methods.rb
+++ methods.rb
@@ -59,3 +59,70 @@ def get_teachers_list_by_letter(letter, client)
     end
   end
 end
+
+def add_md5_column(client)
+  f = "ALTER TABLE teachers_davi_mattos ADD md5 char(32) null;"
+  client.query(f)
+end
+def set_md5(client)
+  f = "SELECT * FROM teachers_davi_mattos;"
+  results = client.query(f).to_a
+  if results.count.zero?
+    puts "No teacher was found!"
+  else
+    results.each do |row|
+      md5 = Digest::MD5.new
+      middle_name = row['middle_name'].nil? ? '' : row['middle_name']
+      str = "#{row['first_name']}#{row['middle_name']}#{row['last_name']}#{row['birth_date']}#{row['subject_id']}#{row['current_age']}"
+      md5 << str
+      f = "UPDATE teachers_davi_mattos SET md5 = '#{md5.hexdigest}' WHERE id = #{row['id']}"
+      client.query(f)
+    end
+  end
+end
+
+def get_class_info(id, client)
+  f = "select
+        cl.name,
+        CONCAT(te.first_name, ' ', COALESCE(te.middle_name, ''), ' ', te.last_name) as teacher_name,
+        CONCAT(te_r.first_name, ' ', COALESCE(te_r.middle_name, ''), ' ', te_r.last_name) as responsible_teacher_name
+        from teachers_classes_davi_mattos tecl
+          JOIN teachers_davi_mattos te
+          ON te.id = tecl.teacher_id
+        JOIN classes_davi_mattos cl
+          ON cl.id = tecl.class_id
+        JOIN teachers_davi_mattos te_r
+          ON te_r.id = cl.responsible_teacher_id
+        WHERE cl.id = #{id};"
+  results = client.query(f).to_a
+  if results.count.zero?
+    puts "No class with id #{id} was found"
+  else
+    str = "Class Name: #{results[0]['name']} \nResponsible Teacher: #{results[0]['responsible_teacher_name']} \nInvolved teachers: \n"
+    count = 0
+    results.each do |row|
+      count +=1
+      str += "#{count} - #{row['teacher_name']}\n"
+    end
+    str
+  end
+end
+
+def get_teachers_by_year(year, client)
+  f = "SELECT
+        CONCAT(te.first_name, ' ', COALESCE(te.middle_name, ''), ' ', te.last_name) as teacher_name
+        FROM teachers_davi_mattos te
+        WHERE year(te.birth_date) = #{year};"
+  results = client.query(f)
+  if results.count.zero?
+    puts "No teacher born in #{year} was found!"
+  else
+    str = "Teachers born in #{year}:\n"
+    count = 0
+    results.each do |row|
+      count += 1
+      str += "#{count} - #{row['teacher_name']}\n"
+    end
+    str
+  end
+end
